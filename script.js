b2f95b42bc75a0435001e3c7fac3c8955a12dc55
Tammy Gagliano <47534603+TammyGagliano@users.noreply.github.com>
2021-09-24 17:08:50 -0500

    updates to add variables

---
+++ script.js
@@ -0,0 +1,13 @@
+//create variables 
+var appointText = "";
+var appointTime = "";
+var currentDate = "";
+var currentTime = "";
+var currentContainer = "";
+var tempArray = [];
+var storedAppointments; 
+var returnedAppointments;
+
+$(window).on("load", function(){
+
+}
